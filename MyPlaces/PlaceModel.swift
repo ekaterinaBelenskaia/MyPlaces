72cc8f49e179f1568048ff9fb66150a38d2c6419
Ekaterina <belenskaya95@gmail.com>
2022-09-10 12:15:04 +0200

    DEV-121212: Added a new method cityByPlace

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -12,17 +12,22 @@ struct Place {
         var places = [Place]()
 
         for place in restarauntNames {
-            places.append(Place(name: place, location: "Lodz", type: "Restaurant", image: place))
+            places.append(Place(name: place, location: cityByPlace(place: place), type: "Restaurant", image: place))
         }
         
-        if Place.restarauntNames.contains("Танчао") {
-            places[6].location = "Brest" 
-        }
-        
-        if Place.restarauntNames.contains("Хинкальная") {
-            places[7].location = "Brest"
-        }
         return places
+        
+    }
+    static func cityByPlace(place: String) -> String {
+        var ret: String = ""
+        if (place == "Танчао") {
+            ret = "Brest"
+        } else if (place == "Хинкальная") {
+            ret = "Brest"
+        } else {
+            ret = "Lodz"
+        }
+        return ret
     }
 }
 
